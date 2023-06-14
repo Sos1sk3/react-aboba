05f74d7428a50befac1fa2239e68b474a9172bd6
Vadim_Podmarkov <podmarkov2003@gmail.com>
2023-06-14 23:20:04 +0300

    Commit.re3

--- src/pages/Autorization.js
+++ src/pages/Autorization.js
@@ -3,62 +3,75 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Autorization() {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [users, setUsers] = useState([]);
-    const [user, setUser] = useState(null);
-    
-    useEffect(() => {
-    axios.get('https://localhost:7241/api/account').then((res) => {
-    setUsers(res.data);
-    }).catch((err) => {
-    console.error(err);
-    alert('Error fetching users');
-    });
-    }, []);
-    
-    const handleLogin = (e) => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://localhost:7241/api/account')
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Error fetching users');
+      });
+  }, []);
+
+  const handleLogin = (e) => {
     e.preventDefault();
     const authenticatedUser = users.find((u) => u.login === login && u.password === password);
     if (!authenticatedUser) {
-    alert('Incorrect username or password');
-    return;
+      setErrorMessage('Неверный логин или пароль');
+      return;
     }
     localStorage.setItem('userId', authenticatedUser.id);
     localStorage.setItem('userLogin', login);
     setUser(authenticatedUser);
     window.location.href = '/';
-    };
-    
-    useEffect(() => {
+  };
+
+  useEffect(() => {
     document.querySelector('.overlay').style.display = 'block';
-    }, []);
-    
-    return (
+  }, []);
+
+  return (
     <div className="overlay">
-    <div className="Login">
+      <div className="Login">
         <Link to="/">
-        <h2 className="Link">вернуться назад</h2>
+          <h2 className="Link">вернуться назад</h2>
         </Link>
-    <h2>Авторизация</h2>
-    <Link to="/Registration" className="Link">
-        
-    <span>Хотите зарегестрироваться?</span>
-    </Link>
-    
-    <div className="login">
-    <input placeholder="Логин..." value={login} onChange={(e) => setLogin(e.target.value)} />
+        <h2>Авторизация</h2>
+        <Link to="/Registration" className="Link">
+          <span>Хотите зарегистрироваться?</span>
+        </Link>
+
+        <div className="login">
+          <input
+            placeholder="Логин..."
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </div>
+        <div className="login">
+          <input
+            placeholder="Пароль..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        {errorMessage && <p className="error">{errorMessage}</p>}
+
+        <button className="logBtn" onClick={handleLogin}>
+          Войти
+        </button>
+      </div>
     </div>
-    <div className="login">
-    <input placeholder="Пароль..." value={password} onChange={(e) => setPassword(e.target.value)} />
-    </div>
-    
-    <button className="logBtn" onClick={handleLogin}>
-    Войти
-    </button>
-    
-    </div>
-    </div>
-    );
-    }
-    export default Autorization;
+  );
+}
+
+export default Autorization;
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -3,66 +3,91 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function Registration() {
-const [email, setEmail] = React.useState('');
-const [password, setPassword] = React.useState('');
-const [Steam, setSteam] = React.useState('');
-const [name, setName] = React.useState('');
-const [login, setLogin] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [Steam, setSteam] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [login, setLogin] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
 
+  const handleLogin1 = (e) => {
+    e.preventDefault();
 
+    // Проверка наличия значений в обязательных полях
+    if (!email || !password || !name || !login || !Steam) {
+      setErrorMessage('Заполните все поля');
+      return;
+    }
 
-const handleLogin1 = (e) => {
-e.preventDefault();
-axios.post('https://localhost:7241/api/account', {
-email,
-password,
-name,
-login,
-Steam,
-}).then((res) => {
-localStorage.setItem('token', res.data.token);
-window.location.href = '/login';
-}).catch((err) => {
-console.error(err);
-alert('Registration Error');
-});
-};
+    axios
+      .post('https://localhost:7241/api/account', {
+        email,
+        password,
+        name,
+        login,
+        Steam,
+      })
+      .then((res) => {
+        localStorage.setItem('token', res.data.token);
+        window.location.href = '/login';
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Registration Error');
+      });
+  };
 
-
-return (
-<div className="overlay">
-<div className="Login">
-<Link to="/login">
-        <h2>вернуться назад</h2>
+  return (
+    <div className="overlay">
+      <div className="Login">
+        <Link to="/login">
+          <h2>вернуться назад</h2>
         </Link>
-<h2>Регистрация</h2>
-<div className="login">
-<input placeholder="Логин" value={login} onChange={(e) => setLogin(e.target.value)} />
-</div>
-<div className="login">
-<input placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
-</div>
-<div className="login">
-<input placeholder="Почта" value={email} onChange={(e) => setEmail(e.target.value)} />
-</div>
-<div className="login">
-<input placeholder="Ссылка на профиль Steam " value={Steam} onChange={(e) => setSteam(e.target.value)} />
-</div>
-<div className="login">
-<input placeholder="Имя" value={name} onChange={(e) => setName(e.target.value)} />
-</div>
+        <h2>Регистрация</h2>
+        <div className="login">
+          <input
+            placeholder="Логин"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </div>
+        <div className="login">
+          <input
+            placeholder="Пароль"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="login">
+          <input
+            placeholder="Почта"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="login">
+          <input
+            placeholder="Ссылка на профиль Steam"
+            value={Steam}
+            onChange={(e) => setSteam(e.target.value)}
+          />
+        </div>
+        <div className="login">
+          <input
+            placeholder="Имя"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
+        {errorMessage && <p className="error">{errorMessage}</p>}
 
-<button className="logBtn" onClick={handleLogin1}>
-
-Зарегестрировать пользователя
-
-</button>
-
-
-
-</div>
-</div>
-);
+        <button className="logBtn" onClick={handleLogin1}>
+          Зарегистрировать пользователя
+        </button>
+      </div>
+    </div>
+  );
 }
-export default Registration
+
+export default Registration;
